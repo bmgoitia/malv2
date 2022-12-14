76a488b10db82a0e04b07a9585bd17570045ce6b
bmgoitia <65491589+bmgoitia@users.noreply.github.com>
2022-12-14 11:09:29 +0100

    Buttons behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 
+
 window.onload = function() {
  
-    $('#op_11').addClass("op_active");
-    $('#op_31').addClass("op_active");
+    
+    $('#op_11').addClass("initial");
+    $('#op_31').addClass("initial");
+
+    
  
 
 
@@ -14,31 +18,34 @@ window.onload = function() {
      $('.alt_cont4').addClass( "not_visible" );
 
      $('.lv_op').click(function(){
-        var mId = $(this).attr('id');
+
+        $('#op_11').removeClass("initial");
+        $('#op_31').removeClass("initial");
         
-        if(mId == "op_12"){
-           
-            $( "#op_11" ).removeClass( "op_active" );
-            $( "#op_12" ).addClass( "op_active" )
+        var mId = $(this).attr('id');      
+        
+        if(mId == "op_12"){           
+            $( "#op_12" ).removeClass( "op_active" );
+            $( "#op_11" ).addClass( "op_active" )
             $( ".alt_cont1" ).removeClass( "visible" ).addClass( "not_visible" );
             $( ".alt_cont2" ).removeClass( "not_visible" ).addClass( "visible" );        
             
             
         } else if(mId == "op_11"){
-            $( "#op_12" ).removeClass( "op_active" );
-            $( "#op_11" ).addClass( "op_active" )
+            $( "#op_11" ).removeClass( "op_active" );
+            $( "#op_12" ).addClass( "op_active" )
             $( ".alt_cont2" ).removeClass( "visible" ).addClass( "not_visible" );
             $( ".alt_cont1" ).removeClass( "not_visible" ).addClass( "visible" );
 
         } else if(mId == "op_32"){
-            $( "#op_31" ).removeClass( "op_active" );
-            $( "#op_32" ).addClass( "op_active" )
+            $( "#op_32" ).removeClass( "op_active" );
+            $( "#op_31" ).addClass( "op_active" )
             $( ".alt_cont3" ).removeClass( "visible" ).addClass( "not_visible" );
             $( ".alt_cont4" ).removeClass( "not_visible" ).addClass( "visible" );  
 
         } else if(mId == "op_31"){
-            $( "#op_32" ).removeClass( "op_active" );
-            $( "#op_31" ).addClass( "op_active" )
+            $( "#op_31" ).removeClass( "op_active" );
+            $( "#op_32" ).addClass( "op_active" )
             $( ".alt_cont4" ).removeClass( "visible" ).addClass( "not_visible" );
             $( ".alt_cont3" ).removeClass( "not_visible" ).addClass( "visible" ); 
         }
